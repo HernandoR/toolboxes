ab93e3d2e2129027efe0bd99a554d2eac973bb8f
HernandoR./ <lzhen.dev@outlook.com>
2022-09-18 02:17:29 +0000

    set free of proxy on git

--- scripts/bash/config-gith.sh
+++ scripts/bash/config-gith.sh
@@ -1,6 +1,6 @@
 git config --global user.email "lzhen.dev@outlook.com"
-git config --global user.name "LiuZhen"
+git config --global user.name "HernandoR"./
 
 # git config --global http.proxy "http://127.0.0.1:7890"
-git config --global http.proxy "http://192.168.58.1:7890"
-git config --global socks.proxy "http://192.168.58.1:7891"
+# git config --global http.proxy "http://192.168.58.1:7890"
+# git config --global socks.proxy "http://192.168.58.1:7891"
